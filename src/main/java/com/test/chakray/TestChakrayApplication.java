0ce7b9c2a64edc94ec538a1716070e14b4c85fda
Roberto <sistemas4@ddvc.mx>
2025-10-22 09:14:05 -0600

    Inicio del test api chakray

---
+++ src/main/java/com/test/chakray/TestChakrayApplication.java
@@ -0,0 +1,13 @@
+package com.test.chakray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestChakrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestChakrayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/chakray/TestChakrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.chakray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestChakrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
